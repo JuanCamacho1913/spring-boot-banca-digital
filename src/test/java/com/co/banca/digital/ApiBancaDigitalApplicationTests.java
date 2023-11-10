3894c10cb1f02bd582c74420b8531585e0b9674f
¨juan <jecb1913@gmail.com>
2023-11-10 13:59:03 -0500

    Inicio del proyecto banca digital.

---
+++ src/test/java/com/co/banca/digital/ApiBancaDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.banca.digital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBancaDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
